46ce25ff4764709641de78e4cdf1ebb2ee2518c7
Nikki M <nrmonegain@gmail.com>
2018-07-08 05:01:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName) {
       cart.splice(i, 1) 
+      console.log(cart)
   } else {
     return "That item is not in your cart."
     }
